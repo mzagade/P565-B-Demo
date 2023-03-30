4678a9cf3a4e81134b73d915ba1ed2e4b40ec369
Manas Zagade <mzagade@iu.edu>
2023-03-30 19:45:37 -0400

    db added

--- src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
+++ src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
@@ -25,15 +25,19 @@ public class CustomerController {
     @PostMapping
     public int create(@Valid  @RequestBody Customer customer){
 
-        return repository.create(customer);
+       Customer addedcustomer =  repository.save(customer);
+       return addedcustomer.getId();
     }
     @PutMapping("/{id}")
     public void update(@Valid @RequestBody Customer customer,@PathVariable int id){
-        repository.update(customer,id);
+        customer.setId(id);
+        repository.save(customer);
     }
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id){
-        repository.delete(id);
+        Customer customer = new Customer();
+        customer.setId(id);
+        repository.delete(customer);
     }
        
 }
--- src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
+++ src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
@@ -1,53 +1,9 @@
 package edu.iu.p565.customerservice.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import edu.iu.p565.customerservice.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import edu.iu.p565.customerservice.model.Customer;
-
 @Repository
-public class CustomerRepository {
-    private List<Customer> customers = new ArrayList<>();  
-    
-    public List<Customer> findAll(){
-        return customers;
-    }
-
-    public int create(Customer customer){
-        int id = customers.size() + 1;
-        customer.setId(id);
-        customers.add(customer);
-        return id;
-    }
-
-    public void update(Customer customer , int id)
-    {
-        Customer x = getCustomerById(id);
-        if(x!=null)
-        {
-            x.setName(customer.getName());
-            x.setEmail(customer.getEmail());
-
-        }else{
-            throw new IllegalStateException("No such customer");
-        }
-
-    }
-
-    private Customer getCustomerById(int id)
-    {return customers.stream().filter(x->x.getId()==id).findAny().orElse(null);
-    }
-
-    public void delete(int id)
-    {
-        Customer x = getCustomerById(id);
-        if(x!=null)
-        {
-            customers.remove(x);
-
-        }
-
-    }
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
